05be58332a638aff440ac440d0fe7a4a6082c2d8
Cristina <mihailescuc@outlook.com>
2022-07-09 00:57:14 +0300

    Disable wallet address input

--- src/components/Widget/FaucetWidget.tsx
+++ src/components/Widget/FaucetWidget.tsx
@@ -1,14 +1,13 @@
 import { Button } from '@chakra-ui/button';
-import { Text } from '@chakra-ui/layout';
 import { useState } from 'react';
 import { Field } from '../Field';
 import { WidgetBodyWrapper, WidgetIcon, WidgetTitle, WidgetWrapper } from './components';
 import { FiChevronsDown } from 'react-icons/fi';
 import { SelectToken } from '../SelectToken';
 import { tokens, Tokens } from './types';
+import { Text } from '@chakra-ui/layout';
 
 export function FaucetWidget() {
-    const [walletAddress, setWalletAddress] = useState<string>('');
     const [token, setToken] = useState<Tokens>();
 
     const buttonText = !token ? 'Select a token' : 'Send';
@@ -23,6 +22,18 @@ export function FaucetWidget() {
                 <Field label='Token' value='10' placeholder='10' isDisabled type='number'>
                     <SelectToken<Tokens> options={tokens} value={token} onChange={setToken} />
                 </Field>
+
+                <WidgetIcon icon={FiChevronsDown} />
+
+                <Field
+                    label='Wallet address'
+                    value='0xca056Fc710F638d48FF6129C9F57FD691b2c73f6'
+                    placeholder='10'
+                    isDisabled
+                />
+                <Text color='primary.medium' variant='small'>
+                    Have fun!
+                </Text>
             </WidgetBodyWrapper>
             <Button variant='purple' isDisabled={buttonText !== 'Send'}>
                 {buttonText}
