b69c6283d68a9805ceb244c52facf554b07f08cd
Cristina <mihailescuc@outlook.com>
2022-08-27 19:31:20 +0300

    Fix balance comparison bug

--- src/components/Widgets/components/FaucetWidgetButton.tsx
+++ src/components/Widgets/components/FaucetWidgetButton.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonProps } from '@chakra-ui/button';
 import { useAccount } from 'wagmi';
 import { useTokenBalance } from 'src/web3';
+import Big from 'big.js';
 import { Tokens } from '../types';
 
 export function FaucetWidgetButton({
@@ -15,7 +16,7 @@ export function FaucetWidgetButton({
     const { isConnected } = useAccount();
     const { data } = useTokenBalance(token?.value);
 
-    if (isConnected && data && data.value.gt('10')) {
+    if (isConnected && data && new Big(data?.formatted || '0').gt('10')) {
         return (
             <Button variant="purple" isDisabled>
                 You have enough tokens
